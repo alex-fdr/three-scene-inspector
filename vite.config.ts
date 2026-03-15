49ecb6698507c44528ea9e94948bf0ae4aef163d
alex-fdr <alexfv01@gmail.com>
2026-03-15 19:40:42 +0200

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dts from 'vite-plugin-dts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  plugins: [
+    dts({
+      include: ['src'],
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.json',
+    }),
+  ],
   resolve: {
     dedupe: ['three'],
   },
